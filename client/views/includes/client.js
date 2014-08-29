9e341cc8a05d7143676224c88809224654fd3197
Devon Hicks <devon@devonhicks.com>
2014-08-29 12:29:40 -0400

    Version 1.0, meant for local download and running until Meteor update issues are sorted

--- client/views/includes/client.js
+++ client/views/includes/client.js
@@ -5,25 +5,44 @@
 
  Template.header.events({
     'click #sktClick': function (evt) {
+
+        $(".sktnf").removeClass("hide");
+        $(".sktnf").addClass("sktnfs");
+        $( ".sktnf" ).hide( 3500 );
+
         $('html, body').animate({
         scrollTop: $("#sktnews").offset().top
     }, 2000);
+
   },
 
 
   'click #boarderClick': function (evt) {
+
+        $(".sbf").removeClass("hide");
+        $(".sbf").addClass("sbfs");
+        $( ".sbf" ).hide( 3500 );
+
         $('html, body').animate({
         scrollTop: $("#skateboarders").offset().top
     }, 2000);
   },
 
    'click #boardClick': function (evt) {
+
+         $(".bdf").removeClass("hide");
+        $(".bdf").addClass("bdfs");
+        $( ".bdf" ).hide(4500 );
         $('html, body').animate({
         scrollTop: $("#boarddesign").offset().top
     }, 2000);
   },
 
    'click #contactClick': function (evt) {
+
+         $(".cuf").removeClass("hide");
+        $(".cuf").addClass("cufs");
+        $( ".cuf" ).hide( 5500 );
         $('html, body').animate({
         scrollTop: $("#contactus").offset().top
     }, 2000);
